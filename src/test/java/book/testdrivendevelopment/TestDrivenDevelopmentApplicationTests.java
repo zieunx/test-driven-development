925768ed582fede403b2da8a7a7662436a84272f
zieunx <wldms_1217@naver.com>
2023-01-04 21:25:14 +0900

    init project

---
+++ src/test/java/book/testdrivendevelopment/TestDrivenDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package book.testdrivendevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDrivenDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
